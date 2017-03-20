af7c9a13d0675316b3a3876ef4e0dd60d0bf3604
cyrixhero <hacker@cyrixhero.net>
2017-03-20 20:43:36 +0800

    Upload emacs.d

---
+++ emacs.d/elpa/dactyl-mode-20140906.1025/dactyl-mode-autoloads.el
@@ -0,0 +1,22 @@
+;;; dactyl-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "dactyl-mode" "dactyl-mode.el" (22734 55497
+;;;;;;  190447 146000))
+;;; Generated autoloads from dactyl-mode.el
+
+(autoload 'dactyl-mode "dactyl-mode" "\
+Major mode for editing Pentadactyl configuration files.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dactyl-mode-autoloads.el ends here
---
+++ emacs.d/elpa/restart-emacs-20170313.939/restart-emacs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "restart-emacs" "20170313.939" "Restart emacs from within emacs" 'nil :url "https://github.com/iqbalansari/restart-emacs" :keywords '("convenience"))
