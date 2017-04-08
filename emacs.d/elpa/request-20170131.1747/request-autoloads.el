ebe9d70a17d4d0fb397d74265c38ae2f47323e3e
cyrixhero <hacker@cyrixhero.net>
2017-04-09 00:03:19 +0800

    Remove emacs.d

--- emacs.d/elpa/request-20170131.1747/request-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; request-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("request.el") (22734 47450 874727 112000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; request-autoloads.el ends here
--- emacs.d/elpa/restart-emacs-20170313.939/restart-emacs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "restart-emacs" "20170313.939" "Restart emacs from within emacs" 'nil :url "https://github.com/iqbalansari/restart-emacs" :keywords '("convenience"))
